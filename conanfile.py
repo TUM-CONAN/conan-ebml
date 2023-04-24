b422621742300288d0bd90b0ba140142c18c0621
Ulrich Eck <ulrich.eck@tum.de>
2023-04-24 17:23:56 +0200

    new package for conan2: ebml

--- conanfile.py
+++ conanfile.py
@@ -1,40 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanException
-import os
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
+from conan.tools.scm import Git
+from conan.tools.files import load, update_conandata, copy, collect_libs
 
-def sort_libs(correct_order, libs, lib_suffix='', reverse_result=False):
-    # Add suffix for correct string matching
-    correct_order[:] = [s.__add__(lib_suffix) for s in correct_order]
-
-    result = []
-    for expectedLib in correct_order:
-        for lib in libs:
-            if expectedLib == lib:
-                result.append(lib)
-
-    if reverse_result:
-        # Linking happens in reversed order
-        result.reverse()
-
-    return result
 
 class LibnameConan(ConanFile):
     name = "ebml"
-    version = "1.4.0"
-    description =   "a C++ libary to parse EBML files."
+    version = "1.4.4"
+    description = "a C++ libary to parse EBML files."
     # topics can get used for searches, GitHub topics, Bintray tags etc. Add here keywords about the library
-    topics = ("conan", "corrad", "magnum", "filesystem", "console", "environment", "os")
+    topics = ("conan", "container", "video")
     url = "https://github.com/TUM-CONAN/conan-ebml"
     homepage = "http://matroska-org.github.io/libebml/"
     author = "ulrich eck"
-    license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
+    license = "MIT"
     exports = ["LICENSE.md"]
     exports_sources = ["CMakeLists.txt", "patches/*"]
-    generators = "cmake"
-    short_paths = True  # Some folders go out of the 260 chars path length scope (windows)
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
@@ -47,55 +31,49 @@ class LibnameConan(ConanFile):
         "fPIC": True,
     }
 
-    # Custom attributes for Bincrafters recipe conventions
-    _source_subfolder = "source_subfolder"
-    _build_subfolder = "build_subfolder"
 
-    requires = (
-    )
+    def export(self):
+        update_conandata(self, {"sources": {
+            "commit": "release-{}".format(self.version),
+            "url": "https://github.com/Matroska-Org/libebml.git"
+            }}
+            )
 
-    # temporary until release fixes interconnect issues on windows/release builds
-    scm = {
-        "type": "git",
-        "subfolder": _source_subfolder,
-        "url": "https://github.com/Matroska-Org/libebml.git",
-        "revision": "release-%s" % (version),
-        "submodule": "recursive",
-    }
+    def source(self):
+        git = Git(self)
+        sources = self.conan_data["sources"]
+        git.clone(url=sources["url"], target=self.source_folder, args=["--recursive"])
+        git.checkout(commit=sources["commit"])
 
-    def config_options(self):
-        if self.settings.os == 'Windows':
-            del self.options.fPIC
-
-    # def configure(self):
-    #     if self.settings.compiler == 'Visual Studio' and int(self.settings.compiler.version.value) < 14:
-    #         raise ConanException("{} requires Visual Studio version 14 or greater".format(self.name))
-
-    def _configure_cmake(self):
-
-        cmake = CMake(self)
+    def generate(self):
+        tc = CMakeToolchain(self)
 
         def add_cmake_option(option, value):
             var_name = "{}".format(option).upper()
             value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str 
-            cmake.definitions[var_name] = var_value
+            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str
+            tc.variables[var_name] = var_value
 
         for option, value in self.options.items():
             add_cmake_option(option, value)
 
-        cmake.configure(build_folder=self._build_subfolder)
+        tc.generate()
 
-        return cmake
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def layout(self):
+        cmake_layout(self, src_folder="source_subfolder")
 
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
+        copy(self, pattern="LICENSE", dst="licenses", src=self.source_folder)
+        cmake = CMake(self)
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs = collect_libs(self)
